b4824a7945536e1c87716e71d877ddcfee5f8836
Saurabh Sunny <skumar@aperturecvo.com>
2020-10-11 18:11:44 +0530

    Updates

---
+++ precache-manifest.79af6019e02d1b60db9bf388ea632bb4.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66928de351f0afbdc3e1ab2b4ba0a90",
+    "url": "/campk12/index.html"
+  },
+  {
+    "revision": "dbee046d91d047d98b4a",
+    "url": "/campk12/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7e4d6c60cc9504326b4a",
+    "url": "/campk12/static/css/main.d6c594f0.chunk.css"
+  },
+  {
+    "revision": "dbee046d91d047d98b4a",
+    "url": "/campk12/static/js/2.c4c8d15a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/campk12/static/js/2.c4c8d15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4d6c60cc9504326b4a",
+    "url": "/campk12/static/js/main.8449d2a7.chunk.js"
+  },
+  {
+    "revision": "6323c128c90c38ea9f22",
+    "url": "/campk12/static/js/runtime-main.daac7c88.js"
+  },
+  {
+    "revision": "6a25b013d8c19ddd002f898928052314",
+    "url": "/campk12/static/media/android.6a25b013.jpg"
+  },
+  {
+    "revision": "a94d81157be404c3d5483e4fe7d49fa9",
+    "url": "/campk12/static/media/arvr.a94d8115.svg"
+  },
+  {
+    "revision": "61e738c13ff9fcde9d43be12f5e855f4",
+    "url": "/campk12/static/media/bg-header.61e738c1.svg"
+  },
+  {
+    "revision": "166fffbede52a65610266661254d9450",
+    "url": "/campk12/static/media/calander.166fffbe.svg"
+  },
+  {
+    "revision": "071e05b96fc6518d2d2f6281a8e782b0",
+    "url": "/campk12/static/media/camp-k-12-logo.071e05b9.svg"
+  },
+  {
+    "revision": "0e9b987e3a3a9dbef8e2f8daf9362110",
+    "url": "/campk12/static/media/coin.0e9b987e.svg"
+  },
+  {
+    "revision": "4451a0d664e7999b27f00eb9fa1e6484",
+    "url": "/campk12/static/media/fill.4451a0d6.svg"
+  },
+  {
+    "revision": "527365b16646d62304dd569c71e957a8",
+    "url": "/campk12/static/media/grade.527365b1.svg"
+  },
+  {
+    "revision": "5549c2623782489eda5f11d102544172",
+    "url": "/campk12/static/media/group-36.5549c262.svg"
+  },
+  {
+    "revision": "4eda1d410148af80e6e262a053cc1a01",
+    "url": "/campk12/static/media/header-photo.4eda1d41.jpg"
+  },
+  {
+    "revision": "8c3d5d63a89092a5d828989b2fc4d9f5",
+    "url": "/campk12/static/media/leaderboard.8c3d5d63.svg"
+  },
+  {
+    "revision": "f2ea4c800b36302cbfb85a9328183910",
+    "url": "/campk12/static/media/leftarrow.f2ea4c80.svg"
+  },
+  {
+    "revision": "7f3988896217ca71dc82c8b8fc6bfec2",
+    "url": "/campk12/static/media/ml.7f398889.svg"
+  },
+  {
+    "revision": "011758a9b47a821c5bcc3d065f439966",
+    "url": "/campk12/static/media/mobileapp.011758a9.svg"
+  },
+  {
+    "revision": "d060ecc802e4d95b80de2c5a16e30da5",
+    "url": "/campk12/static/media/profile.d060ecc8.png"
+  },
+  {
+    "revision": "07b30e0e29436f1340aa3964fbe4af4d",
+    "url": "/campk12/static/media/rightarrow.07b30e0e.svg"
+  },
+  {
+    "revision": "8a1fafc1989ec0413bc9c933051bd846",
+    "url": "/campk12/static/media/shape.8a1fafc1.svg"
+  },
+  {
+    "revision": "c74c0502eb0ae0b5176b5ee683b6d036",
+    "url": "/campk12/static/media/skill-icon.c74c0502.svg"
+  },
+  {
+    "revision": "414c0d9293bbf6a9325152b9d4f6f75e",
+    "url": "/campk12/static/media/stopwatch.414c0d92.svg"
+  },
+  {
+    "revision": "3fb863570e2e5e2f7819c2d81a500f72",
+    "url": "/campk12/static/media/time.3fb86357.svg"
+  },
+  {
+    "revision": "8f61856ac513b2b884fe36226cf9b0fd",
+    "url": "/campk12/static/media/vreal.8f61856a.jpg"
+  },
+  {
+    "revision": "02d82c3c9c026685d2aa44cb885a6835",
+    "url": "/campk12/static/media/webapp.02d82c3c.svg"
+  }
+]);
